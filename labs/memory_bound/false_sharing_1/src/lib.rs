5241977f7da4b4c758987c87e147cedbff19f60e
JonathanHallstrom <lmj.hallstrom@gmail.com>
2024-08-23 10:52:51 +0200

    solve false sharing

--- labs/memory_bound/false_sharing_1/src/lib.rs
+++ labs/memory_bound/false_sharing_1/src/lib.rs
@@ -6,17 +6,24 @@ mod tests;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::thread;
 
+#[repr(align(64))]
+struct Accumulator {
+    data: AtomicU32,
+}
+
 pub fn solution(data: &[u32], thread_count: usize) -> i32 {
     // Using std::atomic counters to disallow compiler to promote `target`
     // memory location into a register. This way we ensure that the store
     // to `target` stays inside the loop.
     let mut accumulators = Vec::with_capacity(thread_count);
-    accumulators.resize_with(thread_count, || AtomicU32::new(0));
+    accumulators.resize_with(thread_count, || Accumulator {
+        data: AtomicU32::new(0),
+    });
     let chunks = data.chunks(data.len() / thread_count);
     thread::scope(|s| {
         // C++ uses `#pragma omp for` which splits into chunks, just like this
         for (idx, chunk) in chunks.enumerate() {
-            let target_acc = &accumulators[idx % thread_count];
+            let target_acc = &accumulators[idx % thread_count].data;
             s.spawn(move || {
                 for v in chunk {
                     // Perform computation on each input
@@ -34,7 +41,7 @@ pub fn solution(data: &[u32], thread_count: usize) -> i32 {
 
     accumulators
         .iter()
-        .map(|v| v.load(Ordering::SeqCst) as usize)
+        .map(|v| v.data.load(Ordering::SeqCst) as usize)
         .sum::<usize>()
         .try_into()
         .unwrap()

7428390b0480655cea5f63dfa946c75662a430e3
JonathanHallstrom <lmj.hallstrom@gmail.com>
2024-08-23 11:12:36 +0200

    solve loop_tiling_1

--- labs/memory_bound/loop_tiling_1/src/lib.rs
+++ labs/memory_bound/loop_tiling_1/src/lib.rs
@@ -16,9 +16,24 @@ pub fn init_matrix(matrix: &mut MatrixOfDoubles) {
 pub fn solution(matrix_in: &MatrixOfDoubles, matrix_out: &mut MatrixOfDoubles) {
     let size = matrix_in.len();
 
-    for i in 0..size {
-        for j in 0..size {
-            matrix_out[i][j] = matrix_in[j][i];
+    // around 8MB chunks, should fit in L3 cache on most machines
+    const N: usize = 1024;
+
+    let mut starti = 0;
+    let mut endi = 0;
+    while starti < size {
+        let mut endj = 0;
+        let mut startj = 0;
+        while startj < size {
+            for i in starti..endi {
+                for j in startj..endj {
+                    matrix_out[i][j] = matrix_in[j][i];
+                }
+            }
+            startj = endj;
+            endj = size.min(endj + N);
         }
+        starti = endi;
+        endi = size.min(endi + N);
     }
 }
